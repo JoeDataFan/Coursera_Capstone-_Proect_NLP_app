32af93a30eeaab4427bf8ca22873ee3313715b7c
JoeDataFan <rj.rubash@gmail.com>
2019-01-09 23:06:57 -0800

    added code to download newest model which is super fast and seems to be the most accurate yet

--- Next_word_prediction/global.R
+++ Next_word_prediction/global.R
@@ -5,5 +5,6 @@ library(tidyverse)
 
 # load model
 #ng.model <- readRDS(file = "./model.data/ngrams.freq.25.rds")
-ng.model <- readRDS(file = "./model.data/ngrams.freq.15.rds")
+#ng.model <- readRDS(file = "./model.data/ngrams.freq.15.rds")
+ng.model <- readRDS(file = "./model.data/ngrams.mod.3.rds")
 #getwd()
