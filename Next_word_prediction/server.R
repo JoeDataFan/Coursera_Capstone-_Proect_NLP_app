5ded90896a021ce306f057765eae9302ca2e47be
JoeDataFan <rj.rubash@gmail.com>
2019-01-13 13:47:26 -0800

    near final state. info about app and how to use it added.

--- Next_word_prediction/server.R
+++ Next_word_prediction/server.R
@@ -8,18 +8,21 @@
 #
 #getwd()
 
+# libraries---------------------------------------------------------------------
 library(shiny)
 
-#functions for later use:
+#functions for later use:-------------------------------------------------------
+# save predicted text data and combine with past data
 saveData <- function(data) {
     #data <- as.data.frame((data))
     if (exists("textString")) {
         textString <<- rbind(textString, data)
     }else{
         textString <<- data
-        }
+    }
 }
 
+# load updated text data
 loadData <- function() {
     if (exists("textString")) {
         textString <- unique(textString)
@@ -29,84 +32,109 @@ loadData <- function() {
     }
 }
 
-    server <- function(input, output, session) {
-        observe({
-            # We'll use the input$controller variable multiple times, so save it as x
-            # for convenience.
-            x <- input$previous.words
-            #pred.string <- ng.15.test[base == str_replace_all(x, "\\s" , "_") %>%
-                                          #str_remove_all(., "_$"), pred]
-            
-            # function to run string through model
-            predict_next_word <- function(prev.words, model = ng.model){
-                prev.words <- trimws(prev.words)
-                n = 5:1
-                patt = sprintf("\\w+( \\w+){0,%d}$", n-1)
-                test.input <- data.table(base = stri_extract(prev.words, regex = patt))
-                test.input <- test.input[, order := length(test.input$base):1]
-                prep.input <- test.input[, base := str_replace_all(test.input$base, "\\s+", "_") %>% 
-                                             #str_replace(., "_$", "") %>% 
-                                             tolower()] 
-                prep.input <- setkey(prep.input, base)
-                prep.input <- prep.input$base
-                results.dt <- model[base %in% prep.input]
-                result <- results.dt[order(-prob)[1], pred][1]
-                result <- if_else(is.na(result), " ", result)
-                print(result)
-            }
-            
-            prediction <- predict_next_word(prev.words = x)
-            
-            # This will change the value of input$inText, based on x
-            updateTextInput(session,
-                            "inText",
-                            value = paste(prediction))
-            
-            # save previous words and prediction when button is clicked
-            textData <- reactive({
-                data <- paste(input$previous.words, input$inText)
-                data <- data.table(returned.text = c(data))
-                })
-            
-            # When the "Yes, that's it!" button is clicked, save the data
-            observeEvent(input$yes, {
-                saveData(textData())
-            }, ignoreNULL = TRUE, ignoreInit = TRUE, once = TRUE)
-            
-            # Show the previous responses
-            # (update with current response when Submit is clicked)
-            observeEvent(input$yes, {
-                output$textString <- renderText({
-                loadData()
-                    })
-                }, ignoreNULL = TRUE, ignoreInit = TRUE, once = TRUE)     
-            
-            observeEvent(input$yes, {
-                updateTextInput(session, "previous.words", value = "")
-                updateTextInput(session, "inText", value = "")
-                }, ignoreNULL = TRUE, ignoreInit = TRUE, once = TRUE)
+
+server <- function(input, output, session) {
+    # code below observes text in previous word input box then automatically runs
+    # through prediction model to give output
+    
+    # Predict next word --------------------------------------------------------
+    # Run previous words through model to give predicted next word
+    observe({
+        # previous words input
+        x <- input$previous.words
+        # function to run previous word string through model
+        predict_next_word <- function(prev.words, model = ng.model){
+            prev.words <- trimws(prev.words)
+            n = 5:1
+            patt = sprintf("\\w+( \\w+){0,%d}$", n-1)
+            test.input <- data.table(base = stri_extract(prev.words, regex = patt))
+            test.input <- test.input[, order := length(test.input$base):1]
+            prep.input <- test.input[, base := str_replace_all(test.input$base, "\\s+", "_") %>% 
+                                         #str_replace(., "_$", "") %>% 
+                                         tolower()] 
+            prep.input <- setkey(prep.input, base)
+            prep.input <- prep.input$base
+            results.dt <- model[base %in% prep.input]
+            result <- results.dt[order(-prob)[1], pred][1]
+            result <- if_else(is.na(result), " ", result)
+            print(result)
+        }
+        
+        prediction <- predict_next_word(prev.words = x)
+        # This will change the value of input$inText, based on x
+        
+        updateTextInput(session,
+                        "inText",
+                        value = paste(prediction))
+        
+        # Combine previous and predicted 
+        # function to save previous words and prediction
+        textData <- reactive({
+            data <- paste(trimws(input$previous.words), input$inText)
+            data <- data.table(returned.text = c(data))
         })
-    }
-    
-    
-    
-    
-    
-    
-#    # Whenever a field is filled, aggregate all form data
-#    formData <- reactive({
-#        data <- sapply(fields, function(x) input[[x]])
-#        data
-#    })
-#    
-#    # When the Submit button is clicked, save the form data
-#    observeEvent(input$submit, {
-#        saveData(formData())
-#    })
-#    
-#    # Show the previous responses
-#    # (update with current response when Submit is clicked)
-#    output$responses <- DT::renderDataTable({
-#        input$submit
-#        loadData()
-#    })     
+        
+        # When the "Yes, that's it!" button is clicked, save the data
+        observe({
+            if(!is.null(input$lastkeypresscode)){
+                if(input$lastkeypresscode == 13){
+                    
+                    #observeEvent(input$yes, {
+                    saveData(textData())
+                }
+            }
+        })
+        
+        
+        # Store and display output ---------------------------------------------
+        
+        # When the button is clicked show the previous responses combined with
+        # current response in the textbox
+        observe({
+            if(!is.null(input$lastkeypresscode)){
+                if(input$lastkeypresscode == 13){
+                    updateTextAreaInput(session,
+                                        "combination",
+                                        value = loadData()
+                    )
+                }
+            }
+        })   
+        
+        # clear text boxes to allow another prediction to be made
+        observe({
+            if(!is.null(input$lastkeypresscode)){
+                if(input$lastkeypresscode == 13){
+                    
+                    #observeEvent(input$yes, {
+                    updateTextInput(session, "previous.words", value = "")
+                    updateTextInput(session, "inText", value = "")}
+            }
+        })
+        
+        # clear stored text data------------------------------------------------
+        # Button to clear stored text data and start over
+        observeEvent(input$clear.text, {
+            textString <<- NULL
+            #output$textString <- renderText({
+            #loadData()
+            updateTextAreaInput(session,
+                                "combination",
+                                value = loadData()
+            )
+        },ignoreNULL = TRUE, ignoreInit = TRUE, once = TRUE)
+        
+        # links to code and data -----------------------------------------------
+        # github link
+        githuburl <- a("Github", href="https://github.com/JoeDataFan/Coursera_Capstone-_Proect_NLP_app")
+        output$Github <- renderUI({
+            tagList("Code to build the model and this application:", githuburl)
+        })
+        data.url <- a("Text_data", href = "https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip")
+        output$Text_data <- renderUI({
+            tagList("Text data used to build the model:", data.url)
+        })
+    })
+}
+
+
--- Next_word_prediction/ui.R
+++ Next_word_prediction/ui.R
@@ -10,13 +10,101 @@
 library(shiny)
 
 ui <- fluidPage(
-    h4('Next Word Prediction Algorithm'),
-    p('Type in the box below and the next word will be predicted'),
-    textInput("previous.words", "Previous words:"),
-    textInput("inText", "Predicted next word:"),
-    actionButton("yes", "Yes, that's it!"),
-    br(),
-    br(),
-    textOutput("textString")
+    # App title ----
+    titlePanel("Next Word Prediction Algorithm"),
+    
+    mainPanel(
+        # Output: Tabset w/ plot, summary, and table ----
+        tabsetPanel(type = "tabs",
+                    tabPanel("Use the app",
+                             # script to allow "enter" key to be used instead of pressing button
+                             tags$script(' $(document).on("keydown", function (e) {
+                                     Shiny.onInputChange("lastkeypresscode", e.keyCode);
+                                     });
+                                     '),
+                             
+                             
+                             # instructions
+                             h4('Type in the box below and the next word will be predicted'),
+                             
+                             # script to allow "enter" key to be used instead of pressing button
+                             tags$script(' $(document).on("keydown", function (e) {
+                                     Shiny.onInputChange("lastkeypresscode", e.keyCode);
+                                     });
+                                     '),
+                             
+                             # text input box for previous words
+                             textInput("previous.words", "Previous words:"),
+                             #div(style="display: inline-block;vertical-align:top; width: 250px;",textInput("previous.words", "Previous words:")),
+                             
+                             # text output giving predicted next word based on previous
+                             textInput("inText", "Predicted next word:"),
+                             #div(style="display: inline-block;vertical-align:top; width: 100px;",textInput("inText", "Next word:")),          
+                             # button to combine previous and predicted then print below. This text string
+                             # continues to grow each time new words are predicted and the button pressed
+                             #h4('Press "ENTER" to add text'),
+                             # growing string of text
+                             
+                             textAreaInput("combination", "Combination (click ENTER):", width = '300px', height = '100px'),
+                             actionButton("clear.text", "Start over")
+                    ),
+                    
+                    tabPanel("Info:", 
+                             h4('How to use this app:'),
+                             h5("This app is intended to make life a little easier by predicting the 
+                                next word you intend to type based on previous words. If the predicted 
+                                word matches your expectations hit ENTER and all text 
+                                will be saved and then displayed below. Continue to follow this method and 
+                                a long string of text will be created fairly quickly."),
+                             br(),
+                             tags$hr(),
+                             h4('Background:'),
+                             h5('This Shiny app is the final product from the Coursera Data Science Specialization
+                                Capstone Project. The goal of this final project was to take a large collect of text
+                                from twitter, blogs and news feeds, create a next word prediction algorithm and then
+                                demonstrate the resulting model in a user-friendly app.'),
+                             p(),
+                             h5('The underlying next word prediction algorithm was built in the following way:'),
+                             tags$ol(
+                                 tags$li('All sources of text data were combined and then randomly split into the following samples:'),
+                                 tags$ul(
+                                     tags$li('25% for training the model (this sample size was the most that could be used for training
+                                             given 16G of RAM)'),
+                                     tags$li('5% for validation of the model'),
+                                     tags$li('5% for final testing of model')
+                                 ),
+                                 tags$li('The training text was then organized by sentences.'),
+                                 tags$li('Sentences were then split into ngrams (2 to 6-word groupings). At this point in processing
+                                        the following items were removed: punctuation, symbols, hyphens, non-ASCII characters,
+                                        hash tags, URLs and words containing numbers. All letters were also converted
+                                         to lowercase.'),
+                                 tags$li('The occurrence of each unique ngram was calculated.'),
+                                 tags$li('The last word of each ngram was then separated from the rest of the words giving 
+                                        two columns PREVIOUS and PREDICTION.'),
+                                 tags$li('The probability of unique PREVIOUS and PREDICTION pairs were then calculated for 
+                                        each unique PREVIOUS word set. Stated more plainly, the final data table shows the
+                                        probability that a word will occur next given specific previous words.'),
+                                 tags$li('The data was then trimmed down by selecting PREDICTION with highest probability for
+                                        each PREVIOUS word set'),
+                                 tags$li('The resulting data table was then filtered to remove profanity from the PREDICTION 
+                                        column.'),
+                                 tags$li('Probabilities were then weighted to give higher preference to larger ngrams; the 
+                                        predicted next word tends to be more accurate given more previous words.'),
+                                 tags$li('Finally, a function was written to take a text string, select the last 5, 4, 
+                                        3, 2 and 1 words, search for these word combinations in the previously built 
+                                        model, return the matching predictions and then select the predicted next 
+                                        word with the highest probability.')
+                                 
+                             ),
+                             h4('Links:'),
+                             uiOutput("Github"),
+                             uiOutput("Text_data")
+                    )
+                    
+        )
     )
-#getwd()
+)
+    
+    
+    
+
